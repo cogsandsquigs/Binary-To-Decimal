3bf1ec9f7dfe040293e4bbd252304637baea2e43
Ian Pratt <ianjdpratt@gmail.com>
2024-06-26 08:31:54 -0500

    Initial Commit

---
+++ Binary To Decimal/Binary_To_DecimalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Binary_To_DecimalApp.swift
+//  Binary To Decimal
+//
+//  Created by Ian Pratt on 6/26/24.
+//
+
+import SwiftUI
+
+@main
+struct Binary_To_DecimalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
